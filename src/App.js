f0c66ebbd6662059d4679ae68402148eb40a777b
Yasir Nisar <yasirnisar07@gmail.com>
2020-06-17 00:14:08 +0300

    New Changes

--- src/App.js
+++ src/App.js
@@ -5,14 +5,21 @@ import { Balance } from "./components/Balance";
 import { IncomeExpense } from "./components/IncomeExpense";
 import { TransactionList } from "./components/TransactionList";
 import { AddTransaction } from "./components/AddTransaction";
-
 import { GlobalProvider } from "./context/GlobalState";
+import GitHubForkRibbon from "react-github-fork-ribbon";
 
 function App({ name }) {
   return (
     <GlobalProvider>
       <Header />
       <div className="container">
+        <GitHubForkRibbon
+          href="https://github.com/yasir8401/expense-tracker-app.git"
+          target="_blank"
+          position="right"
+        >
+          Fork me on GitHub
+        </GitHubForkRibbon>
         <Balance />
         <IncomeExpense />
         <TransactionList />

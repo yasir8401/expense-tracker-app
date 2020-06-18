eb3431a4da562b2f0c2a35f633ce5adcbb51efe0
Yasir Nisar <yasirnisar07@gmail.com>
2020-06-18 20:40:09 +0300

    New Changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,9 @@ import React from "react";
 export const Header = () => {
   return (
     <React.Fragment>
-      <h2>Expense Tracker</h2>
+      <h2>
+        Expense Tracker by <u>Yasir Nisar</u>
+      </h2>
     </React.Fragment>
   );
 };

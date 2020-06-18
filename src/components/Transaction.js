fe858e2c3b4b79f4dd74da6db99f88c085801170
Yasir Nisar <yasirnisar07@gmail.com>
2020-06-18 20:48:58 +0300

    New Changes

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -6,7 +6,7 @@ export const Transaction = ({ transaction }) => {
   const sign = transaction.amount < 0 ? "-" : "+";
   return (
     <li className={transaction.amount < 0 ? "minus" : "plus"}>
-      {transaction.text}{" "}
+      {transaction.text}
       <span>
         {sign}${Math.abs(transaction.amount)}
       </span>
